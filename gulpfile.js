ef86954da11a9525f49d2e1758561690a3074e2f
knock-in <dard.ne@gmail.com>
2016-04-05 16:37:48 +0200

    gulps

--- gulpfile.js
+++ gulpfile.js
@@ -4,12 +4,12 @@ var babel = require('gulp-babel');
 var dir = {
 	src: 'src/**/**.js',
 	dist: 'dist/',
-	test: 'test/*',
-	docs: 'docs/'
-}
+	test: 'test/',
+	docs: 'docs'
+};
 
 gulp.task('default', function() {
 	return gulp.src(dir.src)
 			.pipe(babel())
 			.pipe(gulp.dest(dir.dist));
-})
+});
