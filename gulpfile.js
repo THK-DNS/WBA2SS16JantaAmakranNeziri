13b688642a543b93bad4a7b5af532bd3610cecd8
knock-in <dard.ne@gmail.com>
2016-04-17 12:17:26 +0200

    added ESLint and gulp tasks for linting

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,9 @@
 var gulp = require('gulp');
 var babel = require('gulp-babel');
+var eslint = require('gulp-eslint');
+var watch = require("gulp-watch");
+var shell = require("gulp-shell");
+var gutil = require('gulp-util');
 
 var dir = {
 	src: 'src/**/**.js',
@@ -10,6 +14,23 @@ var dir = {
 
 gulp.task('default', function() {
 	return gulp.src(dir.src)
-			.pipe(babel())
-			.pipe(gulp.dest(dir.dist));
+		.pipe(babel())
+		.pipe(gulp.dest(dir.dist));
+});
+
+gulp.task('lint', function() {
+	return gulp.src(dir.src)
+		.pipe(eslint())
+		.pipe(eslint.format())
+		.pipe(eslint.failAfterError());
+});
+
+gulp.task('autoBuild:lint', function() {
+    return gulp.src(dir.src)
+        .pipe(watch(dir.src).on('change', function(path){
+            gutil.log(`File ${path} has been changed`);
+        }))
+        .pipe(shell(['eslint --quiet src/<%= file.relative %>'], {ignoreErrors: true}))
+        .pipe(babel())
+        .pipe(gulp.dest(dir.dist));
 });
--- src/server/routes/tasks.js
+++ src/server/routes/tasks.js
@@ -1,39 +1,41 @@
 const express = require('express');
 
-const router = express.Router();
+const tasks = new express.Router();
 
 // Initialisiere data objekt
-let data = {
-  "tasks": [{
-    "id": 0,
-    "text": "Mein erster task"
-  }, {
-    "id": 1,
-    "text": "Mein zweiter task"
-  }]
+const data = {
+  tasks: [
+    {
+      id: 0,
+      text: 'Mein erster task',
+    }, {
+      id: 1,
+      text: 'Mein zweiter task',
+    },
+  ],
 };
 
 // tasks ist ein array also können wir auch neue json objekte pushen
-data.tasks.push({"id": 2, "text": "Mein dritter text"});
+data.tasks.push({ id: 2, text: 'Mein dritter text' });
 
 // TODO: Datenbankverbindung herstellen und Daten daraus entnehmen.
 
-router.route('/')
-	// GET /task liefert alle tasks
-	.get((req, res) => {
-		res.send(JSON.stringify(data));
-	})
-	// POST /task erstellt einen neuen task
-	.post((req, res) => {
-		res.end();
-	})
-	// PUT /task Ersetzt unsere taskliste
-	.put((req, res) => {
-		res.end();
-	})
-	// DELETE /task Löscht alle tasks
-	.delete((req, res) => {
-		res.end();
-	});
+tasks.route('/')
+  // GET /task liefert alle tasks
+  .get((req, res) => {
+    res.send(JSON.stringify(data));
+  })
+  // POST /task erstellt einen neuen task
+  .post((req, res) => {
+    res.end();
+  })
+  // PUT /task Ersetzt unsere taskliste
+  .put((req, res) => {
+    res.end();
+  })
+  // DELETE /task Löscht alle tasks
+  .delete((req, res) => {
+    res.end();
+  });
 
-module.exports = router;
+module.exports = tasks;
