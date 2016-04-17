1cd20347e9c6d6a2668822427cb0cdbecc9fc3fc
knock-in <dard.ne@gmail.com>
2016-04-17 13:16:12 +0200

    small task adjustments

--- gulpfile.js
+++ gulpfile.js
@@ -6,31 +6,31 @@ var shell = require("gulp-shell");
 var gutil = require('gulp-util');
 
 var dir = {
-	src: 'src/**/**.js',
-	dist: 'dist/',
-	test: 'test/',
-	docs: 'docs'
+  src: 'src/**/**.js',
+  dist: 'dist/',
+  test: 'test/',
+  docs: 'docs'
 };
 
 gulp.task('default', function() {
-	return gulp.src(dir.src)
-		.pipe(babel())
-		.pipe(gulp.dest(dir.dist));
+  return gulp.src(dir.src)
+    .pipe(babel())
+    .pipe(gulp.dest(dir.dist));
 });
 
 gulp.task('lint', function() {
-	return gulp.src(dir.src)
-		.pipe(eslint())
-		.pipe(eslint.format())
-		.pipe(eslint.failAfterError());
+  return gulp.src(dir.src)
+    .pipe(eslint())
+    .pipe(eslint.format())
+    .pipe(eslint.failAfterError());
 });
 
 gulp.task('autoBuild:lint', function() {
-    return gulp.src(dir.src)
-        .pipe(watch(dir.src).on('change', function(path){
-            gutil.log(`File ${path} has been changed`);
-        }))
-        .pipe(shell(['eslint --quiet src/<%= file.relative %>'], {ignoreErrors: true}))
-        .pipe(babel())
-        .pipe(gulp.dest(dir.dist));
+  return gulp.src(dir.src)
+    .pipe(watch(dir.src).on('change', function(path){
+        gutil.log(`File ${gutil.colors.cyan(path.split('/').slice(-1)[0])} has been changed`);
+        gulp.start('lint'); 
+    }))
+    .pipe(babel())
+    .pipe(gulp.dest(dir.dist));
 });
