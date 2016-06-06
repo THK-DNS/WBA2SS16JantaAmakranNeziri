7494a6f462617d295f875164fb06b5b7afdfd01b
Semra <semra34@hotmail.de>
2016-06-06 11:53:28 +0200

    travelRoute verbessert in Atom

--- src/server/routes/travelRoute.js
+++ src/server/routes/travelRoute.js
@@ -6,41 +6,12 @@ const travelRoute = new express.Router();
 const traveldestinationModel = new traveldestination();
 
 
-travelRoute.route('/')
-
-//GET all traveldestination
-.get((req, res) => {
-      traveldestinationModel.gettraveldestination().then((traveldestination) => {
-        res.json(traveldestination);
-        res.end();
-    });
-  })
-
-//POST  a new traveldestination
-.post((req, res) => {
-  traveldestinationModel.addTraveldestination(req.body).then((user) => {
-    res.json(user);
-    res.end();
-  });
-})
-
-//PUT update a traveldestination
-.put((req, res) => {
-  res.end();
-})
-
-
-//DELETE delete a traveldestination
-.delete((req, res) => {
-  res.end();
-});
-
 traveldestinationRoute.route('/:name')
 
 //GET a traveldestination by its name
 
 .get((req, res) => {
-    
+
     traveldestinationModel.gettraveldestinationByName(req.params.name).then((traveldestination) => {
       res.json(traveldestination);
       res.end();
